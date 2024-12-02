f45e73b5fd22b6d676ea0ee1e745d65237c73adf
Nick <90762430+BassBoulder@users.noreply.github.com>
2024-12-02 12:09:45 +0000

    Completed Remote Control Competition Exercism exercise

---
+++ Java/Exercism/ProductionRemoteControlCar.java
@@ -0,0 +1,26 @@
+public class ProductionRemoteControlCar implements RemoteControlCar, Comparable<ProductionRemoteControlCar> {
+
+    int distance = 0;
+    int victories = 0;
+    
+    public void drive() {
+        distance += 10; 
+    }
+
+    public int getDistanceTravelled() {
+        return distance;
+    }
+
+    public int getNumberOfVictories() {
+        return victories;
+    }
+
+    public void setNumberOfVictories(int numberOfVictories) {
+        this.victories = numberOfVictories;
+    }
+
+    @Override
+    public int compareTo(ProductionRemoteControlCar other){
+        return other.victories - this.victories;
+    }
+}
